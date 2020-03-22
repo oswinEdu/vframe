a9be0fc77f9834710a02bd7106c9699528a931dc
吴建超 <wujianchao@wujianchaodeMacBook-Pro.local>
2020-03-22 17:43:15 +0800

    ok

--- ui/VUI.ts
+++ ui/VUI.ts
@@ -5,6 +5,35 @@ export class VUIIns {
     }
 
 
+
+    public addTestLabel(parent: cc.Node, str: string, ): void {
+        let tnode: cc.Node = new cc.Node();
+        tnode.parent = parent;
+        tnode.color = cc.Color.WHITE;
+        tnode.setContentSize(cc.size(100,20));
+        tnode.anchorX = 0;
+
+        let label: cc.Label = tnode.addComponent(cc.Label);
+        label.string = str;
+        label.fontSize = 20;
+        label.horizontalAlign = cc.Label.HorizontalAlign.LEFT;
+        label.verticalAlign = cc.Label.VerticalAlign.CENTER;
+
+        let widget: cc.Widget = tnode.addComponent(cc.Widget);
+        widget.left = 0;
+        widget.isAlignLeft = true;
+        // widget.top = 0;
+        // widget.isAlignBottom = true;
+        // widget.bottom = 0;
+        // widget.isAlignTop = true;
+
+        // let sp: cc.Sprite = tnode.addComponent(cc.Sprite);
+        // cc.loader.loadRes("texture/singleColor", cc.SpriteFrame, function (err, spriteFrame) {
+        //     sp.spriteFrame = spriteFrame;
+        // });
+        // sp.type = cc.Sprite.Type.SLICED;
+        // sp.sizeMode = cc.Sprite.SizeMode.CUSTOM;        
+    }
 }
 
 
--- vres.ts
+++ vres.ts
@@ -2,11 +2,10 @@
  * 
  */
 
-// import { UILoadIns } from "../ui/UILoad";
-
 namespace vres {
     // prefab 资源路径
     export let PRE_TOAST_1: string = 'prefab/common/toast1.prefab';         //飘窗1
-}
 
+    export let E_EXLINE: string = 'prefab/excelLine';
+}
 window['vres'] = vres;
